a74a9557ccf1c50f12c9dea368482337bf7671e1
Ravi Peters <ravi@ylan.nl>
2026-02-10 13:27:51 +0100

    add hint about output files to dev shell message

--- flake.nix
+++ flake.nix
@@ -92,6 +92,8 @@
             echo ""
             echo "To run the program with sample data:"
             echo "  ./zig-out/bin/test_3d_intersection ./sample_data/9-444-728.city.json NL.IMBAG.Pand.0363100012105349-0 ./sample_data/amsterdam_beemsterstraat_42.gpkg 3.5"
+            echo ""
+            echo "  (a number of .ply files will appear in the current working directory)"
           '';
         };
 
