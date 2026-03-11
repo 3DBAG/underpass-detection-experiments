59cabe64281e4df3b3632729797004b591fa49dc
Ravi Peters <ravi@ylan.nl>
2026-03-11 17:20:02 +0000

    update filepaths in script

--- height_from_streetlidar/plot_z_histogram.py
+++ height_from_streetlidar/plot_z_histogram.py
@@ -9,28 +9,28 @@ from shapely import wkb
 
 CASES = [
     {
-        "las_path": "/Users/ravi/git/zigpip/output/weesperstraat/NL.IMBAG.Pand.0363100012165755.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/weesperstraat/NL.IMBAG.Pand.0363100012165755.gpkg"
+        "las_path": "data/weesperstraat/NL.IMBAG.Pand.0363100012165755.laz",
+        "gpkg_path": "data/weesperstraat/NL.IMBAG.Pand.0363100012165755.gpkg"
     },
     {
-        "las_path": "/Users/ravi/git/zigpip/output/weesperstraat/NL.IMBAG.Pand.0363100012170850.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/weesperstraat/NL.IMBAG.Pand.0363100012170850.gpkg"
+        "las_path": "data/weesperstraat/NL.IMBAG.Pand.0363100012170850.laz",
+        "gpkg_path": "data/weesperstraat/NL.IMBAG.Pand.0363100012170850.gpkg"
     },
     {
-        "las_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012095711.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012095711.gpkg",
+        "las_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012095711.laz",
+        "gpkg_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012095711.gpkg",
     },
     {
-        "las_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012122448.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012122448.gpkg",
+        "las_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012122448.laz",
+        "gpkg_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012122448.gpkg",
     },
     {
-        "las_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012137139.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012137139.gpkg",
+        "las_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012137139.laz",
+        "gpkg_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012137139.gpkg",
     },
     {
-        "las_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012146576.laz",
-        "gpkg_path": "/Users/ravi/git/zigpip/output/beemsterstraat/NL.IMBAG.Pand.0363100012146576.gpkg",
+        "las_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012146576.laz",
+        "gpkg_path": "data/beemsterstraat/NL.IMBAG.Pand.0363100012146576.gpkg",
     },
 ]
 HISTOGRAM_BINS = 100
